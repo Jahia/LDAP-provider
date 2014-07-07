bde2c18468ffb15310fce3fc1f5a382e9c4c371e
Thomas Draier <tdraier@jahia.com>
2014-07-07 11:14:52 +0200

    QA-6527 : unbind configuration that were previously bound to other module versions before trying to use them

---
+++ src/main/java/org/jahia/services/usermanager/ldap/JahiaLDAPConfigManager.java
@@ -0,0 +1,37 @@
+package org.jahia.services.usermanager.ldap;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Unbind configuration at module start/stop
+ */
+public class JahiaLDAPConfigManager {
+    public static final Logger logger = LoggerFactory.getLogger(JahiaLDAPConfigFactory.class);
+    private ConfigurationAdmin configurationAdmin;
+
+    public void start() {
+        unbindConfiguration();
+    }
+
+    public void stop() {
+        unbindConfiguration();
+    }
+
+    private void unbindConfiguration() {
+        try {
+            for (Configuration configuration : configurationAdmin.listConfigurations("(service.factoryPid=org.jahia.services.usermanager.ldap)")) {
+                configuration.setBundleLocation(null);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot unbind configurations",e);
+        }
+    }
+
+    public void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
+        this.configurationAdmin = configurationAdmin;
+    }
+
+}
