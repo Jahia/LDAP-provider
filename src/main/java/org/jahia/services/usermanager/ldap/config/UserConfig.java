b996fd065385c69fc889627d7f76ecb2efef89d9
Thomas Draier <tdraier@jahia.com>
2014-10-23 19:02:48 +0200

    BACKLOG-5344 : use uid.search.attribute as default settings for username mapping

--- src/main/java/org/jahia/services/usermanager/ldap/config/UserConfig.java
+++ src/main/java/org/jahia/services/usermanager/ldap/config/UserConfig.java
@@ -93,7 +93,7 @@ public class UserConfig extends AbstractConfig{
             setSearchWildcardsAttributes(Sets.newHashSet("ou", "cn", "o", "c", "mail", "uid", "uniqueIdentifier", "givenName", "sn", "dn"));
         }
         if(getAttributesMapper().isEmpty()){
-            getAttributesMapper().put("username", "cn");
+            getAttributesMapper().put("username", uidSearchAttribute);
             getAttributesMapper().put("j:firstName", "givenName");
             getAttributesMapper().put("j:lastName", "sn");
             getAttributesMapper().put("j:email", "mail");
