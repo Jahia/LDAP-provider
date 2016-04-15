0169c88879361f78899b0d564c7e6e0e1acf7dc3
Sergiy Shyrkov <sshyrkov@jahia.com>
2016-04-15 11:52:57 +0200

    QA-8407 , QA-8173 - reverted fix "increased default value of maxActive for commons pool from 8 to 50" as under load the commons-pool with more connections behaves badly

--- src/main/java/org/jahia/services/usermanager/ldap/config/AbstractConfig.java
+++ src/main/java/org/jahia/services/usermanager/ldap/config/AbstractConfig.java
@@ -75,7 +75,7 @@ public abstract class AbstractConfig {
     private String ldapConnectPoolMaxSize;
     private String ldapConnectPoolPrefSize;
 
-    private Integer ldapConnectPoolMaxActive = 50;
+    private Integer ldapConnectPoolMaxActive;
     private Integer ldapConnectPoolMaxIdle;
     private Integer ldapConnectPoolMaxTotal;
     private Integer ldapConnectPoolMaxWait;
